fd786529af3c5ee1192025c862f240f10a075dab
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-10 22:23:32 +0300

    Update Navbar.jsx

--- E-commerce-website/frontend/src/components/Navbar.jsx
+++ E-commerce-website/frontend/src/components/Navbar.jsx
@@ -56,16 +56,16 @@ function Navbar() {
 
       <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
         <div className='flex flex-col text-gray-600'>
-          <div onClick={()=> setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer border-b-2'>
+          <div onClick={()=> setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer border-b-2 border-gray-400'>
             <img src={assets.dropdown_icon} className='h-4 rotate-180' alt="dropdown-icon" />
             <p>
               Back
             </p>
           </div>
-          <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border-b-2 ' to='/'>HOME</NavLink>
-          <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border-b-2' to='/collection'>COLLECTION</NavLink>
-          <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border-b-2' to='/about'>ABOUT</NavLink>
-          <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border-b-2' to='/contact'>CONTACT</NavLink>
+          <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border-b-2 border-x-2 border-gray-400' to='/'>HOME</NavLink>
+          <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border-b-2 border-x-2 border-gray-400' to='/collection'>COLLECTION</NavLink>
+          <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border-b-2 border-x-2 border-gray-400' to='/about'>ABOUT</NavLink>
+          <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border-b-2 border-x-2 border-gray-400' to='/contact'>CONTACT</NavLink>
         </div>
       </div>
     </div>
