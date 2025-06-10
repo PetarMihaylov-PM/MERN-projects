e6a80f6c752590d8c629d75087ea826e1383e16c
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-10 20:45:58 +0300

    Update Navbar.jsx

--- E-commerce-website/frontend/src/components/Navbar.jsx
+++ E-commerce-website/frontend/src/components/Navbar.jsx
@@ -26,6 +26,27 @@ function Navbar() {
           <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
         </NavLink>
       </ul>
+
+      <div className='flex items-center gap-6'>
+
+        <img src={assets.search_icon} alt="search-icon" className='w-5 cursor-pointer'/>
+        
+        <div className='group relative'>
+          
+          <img src={assets.profile_icon} alt="profile-icon" className='w-5 cursor-pointer'/>
+
+          <div 
+            className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+              <div className='flex flex-colum gap-2 w-36 py-3 px-5  bg-slate-100 text-gray-500 rounded'>
+                <p className='cursor-pointer hover:text-black'>My profile</p>
+                <p className='cursor-pointer hover:text-black'>Orders</p>
+                <p className='cursor-pointer hover:text-black'>Logout</p>
+              </div>
+          </div>
+
+        </div>
+      
+      </div>
     </div>
   )
 }
