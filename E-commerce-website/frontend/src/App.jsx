08b5ec41b3538b5086f7adf17d065d3db1f027d3
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-10 15:02:01 +0300

    Setting up the Routes for the website

--- E-commerce-website/frontend/src/App.jsx
+++ E-commerce-website/frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Collection from './pages/Collection';
+import Login from './pages/Login';
+import Orders from './pages/Orders';
 
 function App() {
   return (
-    <div>App</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/collection' element={<Collection />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/contact' element={<Contact />}/>
+        <Route path='/product/:productId' element={<Product />}/>
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/placeorder' element={<PlaceOrder />}/>
+        <Route path='/orders' element={<Orders />}/>
+      </Routes>
+      
+    </div>
   )
 }
 
