c097bc71da29bd8077f405a540a99559b7aea505
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-11 22:43:22 +0300

    creating the latestcollection.jsx components, which will render 10 products on the home page

--- E-commerce-website/frontend/src/components/LatestCollection.jsx
+++ E-commerce-website/frontend/src/components/LatestCollection.jsx
@@ -1,13 +1,36 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
 
 function LatestCollection() {
 
   const { products } = useContext(ShopContext);
+  const [latestProducts, setLatestProducts] = useState([]);
 
-  console.log(products)
+  /* for random collection every time
+
+  let firstRandomIndex = Math.floor(Math.random()* (products.length - 11));
+  let secondRandomIndex = firstRandomIndex + 10;
+
+  change useEffect to:
+  useEffect(()=> {
+    setLatestProducts(products.slice(firstRandomIndex, secondRandomIndex));
+  }, []);
+  
+  */
+
+  useEffect(()=> {
+    setLatestProducts(products.slice(0, 10));
+  }, []);
+
   return (
-    <div>
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTION'}/>
+        <p className='w-3/4 m-auto text-xs sm:text md:text-base text-gray-600'>
+          Explore our newest arrivals - stylish, versatile, and made to elevate your everyday look.
+        </p>
+      </div>
       
     </div>
   )
