cd393c62f938f851c448bbcc0ae6bdc9969d6462
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-13 22:31:15 +0300

    Creating the Filter section on the Collection page

--- E-commerce-website/frontend/src/pages/Collection.jsx
+++ E-commerce-website/frontend/src/pages/Collection.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 
 function Collection() {
+
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
   return (
-    <div>Collection</div>
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+
+      {/* Filter Oprtions */}
+      <div className='min-w-60'>
+        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>
+          FILTERS
+        </p>
+        {/* Category Filter */}
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'} sm:block`}>
+          <p className='mb-3 txet-sm font-medium'>CATEGORIES</p>
+          <div className='flex flex-col gap-2 text-sm font-light text-gray-700'>
+            <p className='flex gap-2'></p>
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
