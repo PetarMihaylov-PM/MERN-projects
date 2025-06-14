57f126e496abab35caf4a1ad5cc988de140c22ed
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-14 17:29:11 +0300

    Sorting and Filtering fix.

--- E-commerce-website/frontend/src/pages/Collection.jsx
+++ E-commerce-website/frontend/src/pages/Collection.jsx
@@ -11,6 +11,7 @@ function Collection() {
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
   const [subCategory, setSubCategory] = useState([]);
+  const [sortOption, setSortOption] = useState('relevant');
 
   const toggleCategory = (e) => {
     if(category.includes(e.target.value)){
@@ -32,22 +33,35 @@ function Collection() {
 
   const applyFilter = () => {
 
-    let produtsCopy = products.slice();
+    let productsCopy = products.slice();
 
     if(category.length > 0) {
-      produtsCopy = produtsCopy.filter(item => category.includes(item.category));
+      productsCopy = productsCopy.filter(item => category.includes(item.category));
     }
 
-    setFilterProducts(produtsCopy);
+    if(subCategory.length > 0) {
+      productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory));
+    }
+
+    switch (sortOption){
+      case 'low-high': productsCopy = productsCopy.sort((a, b) => a.price - b.price);
+        setFilterProducts(productsCopy);
+        break;
+
+      case 'high-low': productsCopy = productsCopy.sort((a, b) => b.price - a.price);
+        setFilterProducts(productsCopy);
+        break;
+
+      default:
+        break;
+    }
+
+    setFilterProducts(productsCopy);
   }
 
   useEffect(() => {
-    setFilterProducts(products);
-  }, []);
-
-  useEffect(() => {
     applyFilter();
-  }, [category, subCategory]);
+  }, [category, subCategory, sortOption]);
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -102,7 +116,7 @@ function Collection() {
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'}/>
           {/* Product Sort */}
-          <select className='border-2 border-gray-300 text-sm px-2 cursor-pointer'>
+          <select onChange={(e) => setSortOption(e.target.value)} className='border-2 border-gray-300 text-sm px-2 cursor-pointer'>
             <option value="relevant">Sort by: Relevant</option>
             <option value="low-high">Sort by: Low to High</option>
             <option value="high-low">Sort by: High to Low</option>
