23ea69b91dd180fc8e1cb189dc8d127c2bbfbb88
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-18 18:25:54 +0300

    Rendering the cartItems on the Cart page

--- E-commerce-website/frontend/src/pages/Cart.jsx
+++ E-commerce-website/frontend/src/pages/Cart.jsx
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
 
 function Cart() {
   const { products, currency, cartItems } = useContext(ShopContext);
@@ -20,12 +21,41 @@ function Cart() {
         }
       }
     }
-    console.log(tempData)
+    setCartData(tempData);
 
   },[cartItems]);
 
   return (
-    <div>
+    <div className='border-t pt-14'>
+      
+      <div className='text-2xl mb-3'>
+        <Title text1={'YOURT'} text2={'CART'}/>
+      </div>
+
+      <div>
+        {
+          cartData.map((item, index) => {
+            
+            const productData = products.find(product => product._id === item._id);
+
+            return (
+              <div key={index} className='py-4 border-t text-gray-700 grid grid-cols-[4fr_0.5fr_05.fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img className='w-16 sm:w-20' src={productData.image[0]} alt="product-img" />
+                  <div>
+                    <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                    <div className='flex items-center gap-5 mt-2'>
+                      <p>{currency} {productData.price}</p>
+                      <p className='w-11 text-center px-2 sm:px-3 sm:py-1 border border-gray-400 bg-slate-50'>{item.size}</p>
+                    </div>
+                  </div>
+                </div>
+                <input className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity}/>
+              </div>
+            )
+          })
+        }
+      </div>
 
     </div>
   )
