12c57a3323b3d262917e7f8754638ac79b19a8b7
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-19 16:26:35 +0300

    rendering the products in the order page

--- E-commerce-website/frontend/src/pages/Orders.jsx
+++ E-commerce-website/frontend/src/pages/Orders.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
 
 function Orders() {
+
+  const { currency, products } = useContext(ShopContext);
+
   return (
-    <div>Orders</div>
+    <div className='border-t pt-16'>
+
+      <div className='text-2xl'>
+        <Title text1={'MY'} text2={'ORDERS'}/>
+      </div>
+
+      <div>
+        {
+          products.slice(1,4).map((item, index) => (
+            <div key={index} className='py-4 borde-t text-gray-700 flex flex-row md:flex-row md:items-center md:justify-between gap-4'>
+              <div className='flex item-start gap-6 text-sm'>
+                <img className='w-16 sm:w-20' src={item.image[0]} alt="product-img" />
+                <div>
+                  <p className='sm:text-base font-medium'>
+                    {item.name}
+                  </p>
+                  <div className='flex items-center gap-3 mt-2 text-base text-gray-700'> 
+                    <p className='text-lg'>{currency}{item.price}</p>
+                    <p>Quantity: 1</p>
+                    <p>Size: M</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+
+    </div>
   )
 }
 
--- E-commerce-website/frontend/src/pages/PlaceOrder.jsx
+++ E-commerce-website/frontend/src/pages/PlaceOrder.jsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react'
 import Title from '../components/Title';
 import CartTotal from '../components/CartTotal';
 import { assets } from '../assets/frontend_assets/assets';
+import { useNavigate } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
+import { useContext } from 'react';
 
 function PlaceOrder() {
 
   const [method, setMethod] = useState('cod');
+
+  const { navigate } = useContext(ShopContext);
 
   return (
     <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
@@ -43,18 +48,22 @@ function PlaceOrder() {
 
           {/* Payment method selection */}
           <div className='flex gap-3 flex-col lg:flex-row'>
-            <div onClick={() => setMethod('stripe')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
+            <div onClick={() => setMethod('stripe')} className='flex items-center border border-gray-300 gap-3 p-2 px-3 rounded cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
               <img className='h-5 mx-4' src={assets.stripe_logo} alt="stripe-logo" />
             </div>
-            <div onClick={() => setMethod('razorpay')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''}`}></p>
+            <div onClick={() => setMethod('razorpay')} className='flex items-center border border-gray-300 gap-3 p-2 px-3 rounded  cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full ${method === 'razorpay' ? 'bg-green-400' : ''}`}></p>
               <img className='h-5 mx-4' src={assets.razorpay_logo} alt="razorpay-logo" />
             </div>
-            <div onClick={() => setMethod('cod')} className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
-              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
+            <div onClick={() => setMethod('cod')} className='flex items-center gap-3 border border-gray-300 p-2 px-3 rounded  cursor-pointer'>
+              <p className={`min-w-3.5 h-3.5 border border-gray-300 rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
               <p>CASH ON DELIVERY</p>
             </div>
+          </div>
+
+          <div className='w-full text-end mt-8'>
+            <button onClick={() => navigate('/orders')} className='bg-black text-white px-16 py-3 text-sm'>PLACE ORDER</button>
           </div>
         </div>
 
