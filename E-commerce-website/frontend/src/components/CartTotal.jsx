a2d6b315444ce89334237fb35bca22514ebdd207
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-19 11:17:34 +0300

    Update CartTotal.jsx

--- E-commerce-website/frontend/src/components/CartTotal.jsx
+++ E-commerce-website/frontend/src/components/CartTotal.jsx
@@ -1,12 +1,36 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
 
 const CartTotal = () => {
 
-  const { currency, getCartAmount } = useContext(ShopContext)
+  const { currency, getCartAmount, deliveryFee } = useContext(ShopContext)
 
   return (
-    <div>CartTotal</div>
+    <div className='w-full'>
+      <div className='text-2xl'>
+        <Title text1={'CART'} text2={'TOTAL'}/>
+      </div>
+
+      <div className='flex flex-col gap-2 mt-2 text-sm'>
+        <div className='flex justify-between'>
+          <p>Subtotal</p>
+          <p>{currency} {getCartAmount()}.00</p>
+        </div>
+        <hr />
+
+        <div className='flex justify-between'>
+          <p>Shipping Fee</p>
+          <p>{currency} {deliveryFee}</p>
+        </div>
+        <hr />
+
+        <div className='flex flex-col gap-2 mt-2 text-sm'>
+          <p>Total</p>
+          <p>{currency} {getCartAmount() === 0 ? 0 : getCartAmount() + deliveryFee}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
