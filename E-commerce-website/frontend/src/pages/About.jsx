87bb87511ba6564632e35b51899ad9fe9a4baf26
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-21 22:14:24 +0300

    creating the About page structure

--- E-commerce-website/frontend/src/pages/About.jsx
+++ E-commerce-website/frontend/src/pages/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/frontend_assets/assets'
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'}/>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="about-img" />
+        <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+          <p>Welcome to Forever — your one-stop destination for timeless style and everyday essentials. We believe in combining quality with affordability, offering a carefully curated selection of fashion, accessories, and lifestyle products to suit every taste and occasion.</p>
+          <p>At Forever, our mission is to make shopping easy, enjoyable, and inspiring. Whether you're looking for the latest trends or classic favorites, we're here to help you express your individuality through products that are made to last.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- E-commerce-website/frontend/src/pages/Login.jsx
+++ E-commerce-website/frontend/src/pages/Login.jsx
@@ -4,8 +4,12 @@ function Login() {
 
   const [currentState, setCurrentState] = useState('Sign Up');
 
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <form className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
+    <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className='inline-flex items-center gap-2 mb-2 mt-10'>
         <p className='prata-regular text-3xl'>{currentState}</p>
         <hr className='border-non h-[1.5px] w-8 bg-gray-800'/>
@@ -26,6 +30,9 @@ function Login() {
           <p onClick={() => setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
         }
       </div>
+      <button className='bg-black text-white font-light px-8 py-2 mt-4 cursor-pointer'>
+        {currentState === 'Login' ? 'Sign In' : 'Sign Up'}
+      </button>
     </form>
   )
 }
