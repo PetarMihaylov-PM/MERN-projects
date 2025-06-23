b6fc6546ce81a6ed138145269c7380f29c6aa5f6
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-23 15:07:31 +0300

    Adding the userRegistration logic, validating the input and hashing the password

--- E-commerce-website/backend/controllers/userController.js
+++ E-commerce-website/backend/controllers/userController.js
@@ -1,9 +1,19 @@
-import { response } from "express"
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { response } from "express";
+import userModel from '../models/userModule.js';
+
+
+const createToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET);
+}
+
 
 // Route for user login
 const loginUser = async (req,res) => {
 
-  res.json({msg:'Register API WORKING'});
+  
 
 }
 
@@ -12,7 +22,48 @@ const loginUser = async (req,res) => {
 // Route for user registration
 const registerUser = async (req,res) => {
 
-  res.json({msg:'Register API WORKING'});
+  try {
+
+    const {name, email, password} = req.body;
+
+    // checking if the user already exists
+    const exists = await userModel.findOne({email});
+
+    if(exists){
+      return res.json({success: false, message: "User already exists"});
+    }
+
+    // validating the email and strong password
+    if(!validator.isEmail(email)) {
+      return res.json({success: false, message: "Please enter a valid email"});
+    }
+
+    if(password.length < 8) {
+      return res.json({success: false, message: "Please enter a strong password"});
+    }
+
+    // hasing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new userModel({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+
+    res.json({success: true, token});
+
+  } catch(error) {
+
+    console.log(error);
+    res.json({success:false, message:error.message})
+
+  }
 
 }
 
