9380c85914a256f70cc5722b5add8f49d169d3e7
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-24 15:47:48 +0300

    Update productController.js

--- E-commerce-website/backend/controllers/productController.js
+++ E-commerce-website/backend/controllers/productController.js
@@ -2,7 +2,26 @@
 
 
 // add product
-const addProduct = async(req, res) => {
+const addProduct = async (req, res) => {
+
+  try {
+    
+    const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
+
+    const image1 = req.files.image1[0];
+    const image2 = req.files.image2[0];
+    const image3 = req.files.image3[0];
+    const image4 = req.files.image4[0];
+
+    console.log(name, description, price, category, subCategory, sizes, bestseller);
+    console.log(image1,image2,image3,image4);
+
+    res.json({});
+
+  } catch (error) {
+    console.log(error);
+    res.json({success: false, message: error.message});
+  }
 
 }
 
@@ -10,14 +29,14 @@ const addProduct = async(req, res) => {
 
 
 // list product
-const listProducts = async(req, res) => {
+const listProducts = async (req, res) => {
 
 }
 
 
 
 // remove product
-const removeProduct = async(req, res) => {
+const removeProduct = async (req, res) => {
 
 }
 
@@ -25,8 +44,8 @@ const removeProduct = async(req, res) => {
 
 
 // single product info
-const singleProduct = async(req, res) => {
+const singleProduct = async (req, res) => {
 
 }
 
-export {addProduct, listProducts, removeProduct, singleProduct};
+export { addProduct, listProducts, removeProduct, singleProduct };
