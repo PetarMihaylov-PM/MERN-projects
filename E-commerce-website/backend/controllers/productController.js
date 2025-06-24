5d3f06a9a9be4222e474c83e93a8378dae326263
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-24 15:22:08 +0300

    Create productController.js

---
+++ E-commerce-website/backend/controllers/productController.js
@@ -0,0 +1,30 @@
+
+
+
+// add product
+const addProduct = async(req, res) => {
+
+}
+
+
+
+
+// list product
+const listProducts = async(req, res) => {
+
+}
+
+
+
+// remove product
+const removeProduct = async(req, res) => {
+
+}
+
+
+
+
+// single product info
+const singleProduct = async(req, res) => {
+
+}
