21fdc4ec7fb315175f5d6f37d1fd22fb414552c3
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-25 22:35:39 +0300

    Update productController.js

--- E-commerce-website/backend/controllers/productController.js
+++ E-commerce-website/backend/controllers/productController.js
@@ -54,13 +54,38 @@ const addProduct = async (req, res) => {
 
 // list product
 const listProducts = async (req, res) => {
+  try {
+    
+    const products = await productModel.find({})
+    
 
+    res.json({success: true, products});
+
+  } catch (error) {
+    
+    console.log(error);
+    res.json({success: false, message: error.message});
+
+  }
 }
 
 
 
 // remove product
 const removeProduct = async (req, res) => {
+
+  try {
+    
+    await productModel.findByIdAndDelete(req.body.id);
+
+    res.json({success: true, message: 'Product Removed'})
+
+  } catch (error) {
+    
+    console.log(error);
+    res.json({success: false, message: error.message});
+
+  }
 
 }
 
