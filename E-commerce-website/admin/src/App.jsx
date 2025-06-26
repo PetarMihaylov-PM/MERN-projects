0b17efb0c8ada86534e8de4fd058125e158084e1
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-26 14:49:21 +0300

    Creating the Sidebar routes

--- E-commerce-website/admin/src/App.jsx
+++ E-commerce-website/admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
 
 function App() {
   return (
@@ -10,6 +14,15 @@ function App() {
 
       <div className='flex w-full'>
         <Sidebar />
+        <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+
+          <Routes>
+            <Route path='/add' element={<Add />}/>
+            <Route path='/list' element={<List />}/>
+            <Route path='/orders' element={<Orders />}/>
+          </Routes>
+          
+        </div>
       </div> 
     </div>
   )
--- E-commerce-website/admin/src/components/Sidebar.jsx
+++ E-commerce-website/admin/src/components/Sidebar.jsx
@@ -12,12 +12,12 @@ const Sidebar = () => {
          <p className='hidden md:block'>Add Item</p>
         </NavLink>
 
-        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to={'/add'}>
+        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to={'/list'}>
          <img className='w-5 h-5' src={assets.order_icon} alt="add-icon" />
          <p className='hidden md:block'>List Item</p>
         </NavLink>
 
-        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to={'/add'}>
+        <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to={'/orders'}>
          <img className='w-5 h-5' src={assets.order_icon} alt="add-icon" />
          <p className='hidden md:block'>Orders</p>
         </NavLink>
