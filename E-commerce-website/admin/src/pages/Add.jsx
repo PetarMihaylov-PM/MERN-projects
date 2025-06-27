a7bbc085a9b77ae050fe2d040a0ec1db2860ad32
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-27 16:21:19 +0300

    Creating the file uploading form

--- E-commerce-website/admin/src/pages/Add.jsx
+++ E-commerce-website/admin/src/pages/Add.jsx
@@ -3,15 +3,35 @@ import { assets } from '../assets/assets'
 
 const Add = ({token}) => {
   return (
-    <form>
+    <form className='flex flex-col w-full items-start gap-3'>
       <div>
-        <p>Upload Image</p>
-        <div>
+        <p className='mb-2'>Upload Image</p>
+        <div className='flex gap-2'>
           <label htmlFor='image1'>
-            <img src={assets.upload_area} alt="upload-icon" />
+            <img className='w-20 cursor-pointer' src={assets.upload_area} alt="upload-icon" />
             <input type="file" id='image1' hidden/>
           </label>
+
+          <label htmlFor='image2'>
+            <img className='w-20 cursor-pointer' src={assets.upload_area} alt="upload-icon" />
+            <input type="file" id='image2' hidden/>
+          </label>
+
+          <label htmlFor='image3'>
+            <img className='w-20 cursor-pointer' src={assets.upload_area} alt="upload-icon" />
+            <input type="file" id='image3' hidden/>
+          </label>
+
+          <label htmlFor='image4'>
+            <img className='w-20 cursor-pointer' src={assets.upload_area} alt="upload-icon" />
+            <input type="file" id='image4' hidden/>
+          </label>
         </div>
+      </div>
+
+      <div>
+        <p>Product name</p>
+        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required/>
       </div>
     </form>
   )
