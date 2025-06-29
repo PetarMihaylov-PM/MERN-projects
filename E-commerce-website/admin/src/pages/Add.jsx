b9097571bee81d1532db66540dfd6da3a45bc588
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-29 12:02:03 +0300

    Update Add.jsx

--- E-commerce-website/admin/src/pages/Add.jsx
+++ E-commerce-website/admin/src/pages/Add.jsx
@@ -17,6 +17,10 @@ const Add = ({token}) => {
   const [sizes, setSizes] = useState([]);
 
 
+  const addOrRemoveSize = (size) => {
+    setSizes(prev => prev.includes(size) ? prev.filter(item => item !== size) : [...prev, size]);
+  }
+
 
   return (
     <form className='flex flex-col w-full items-start gap-3'>
@@ -24,22 +28,22 @@ const Add = ({token}) => {
         <p className='mb-2'>Upload Image</p>
         <div className='flex gap-2'>
           <label htmlFor='image1'>
-            <img className='w-20 cursor-pointer' src={assets.upload_area} alt="upload-icon" />
+            <img className='w-20 cursor-pointer' src={image1 ? URL.createObjectURL(image1) : assets.upload_area} alt="upload-icon" />
             <input onChange={(e) => setImage1(e.target.files[0])} type="file" id='image1' hidden/>
           </label>
 
           <label htmlFor='image2'>
-            <img className='w-20 cursor-pointer' src={assets.upload_area} alt="upload-icon" />
+            <img className='w-20 cursor-pointer' src={image2 ? URL.createObjectURL(image2) : assets.upload_area} alt="upload-icon" />
             <input onChange={(e) => setImage2(e.target.files[0])} type="file" id='image2' hidden/>
           </label>
 
           <label htmlFor='image3'>
-            <img className='w-20 cursor-pointer' src={assets.upload_area} alt="upload-icon" />
+            <img className='w-20 cursor-pointer' src={image3 ? URL.createObjectURL(image3) : assets.upload_area} alt="upload-icon" />
             <input onChange={(e) => setImage3(e.target.files[0])} type="file" id='image3' hidden/>
           </label>
 
           <label htmlFor='image4'>
-            <img className='w-20 cursor-pointer' src={assets.upload_area} alt="upload-icon" />
+            <img className='w-20 cursor-pointer' src={image4 ? URL.createObjectURL(image4) : assets.upload_area} alt="upload-icon" />
             <input onChange={(e) => setImage4(e.target.files[0])} type="file" id='image4' hidden/>
           </label>
         </div>
@@ -47,18 +51,18 @@ const Add = ({token}) => {
 
       <div className='w-full'>
         <p className='mb-2'>Product name</p>
-        <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required/>
+        <input onChange={(e) => setName(e.target.value)} value={name} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here' required/>
       </div>
 
       <div className='w-full'>
         <p className='mb-2'>Product description</p>
-        <textarea className='w- full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required/>
+        <textarea onChange={(e) => setDescription(e.target.value)} value={description} className='w- full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required/>
       </div>
 
       <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
         <div>
           <p className='mb-2 text-nowrap'>Product category</p>
-          <select className='w-full px-3 py-2'>
+          <select onChange={(e) => setCategory(e.target.value)} value={category} className='w-full px-3 py-2'>
             <option value="Men">Men</option>
             <option value="Women">Women</option>
             <option value="Kids">Kids</option>
@@ -67,7 +71,7 @@ const Add = ({token}) => {
 
         <div>
           <p className='mb-2'>Sub category</p>
-          <select className='w-full px-3 py-2'>
+          <select onChange={(e) => setSubcategory(e.target.value)} value={subcategory} className='w-full px-3 py-2'>
             <option value="Topwear">Topwear</option>
             <option value="Bottomwear">Bottomwear</option>
             <option value="Winterwear">Winterwear</option>
@@ -76,31 +80,31 @@ const Add = ({token}) => {
 
         <div>
           <p className='mb-2'>Product price</p>
-          <input className='w-full px-3 py-2 sm:w-[120px]' type="number" placeholder='25' />
+          <input onChange={(e) => setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type="number" placeholder='25' />
         </div>
       </div>
 
       <div>
         <p className='mb-2'>Product sizes</p>
         <div className='flex gap-3'>
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+          <div onClick={() => addOrRemoveSize('S')}>
+            <p className={`px-3 py-1 cursor-pointer ${sizes.includes('S') ? 'bg-pink-300' : 'bg-slate-200'}`}>S</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+          <div onClick={() => addOrRemoveSize('M')}>
+            <p className={`px-3 py-1 cursor-pointer ${sizes.includes('M') ? 'bg-pink-300' : 'bg-slate-200'}`}>M</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+          <div onClick={() => addOrRemoveSize('L')}>
+            <p className={`px-3 py-1 cursor-pointer ${sizes.includes('L') ? 'bg-pink-300' : 'bg-slate-200'}`}>L</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+          <div onClick={() => addOrRemoveSize('XL')}>
+            <p className={`px-3 py-1 cursor-pointer ${sizes.includes('XL') ? 'bg-pink-300' : 'bg-slate-200'}`}>XL</p>
           </div>
 
-          <div>
-            <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+          <div onClick={() => addOrRemoveSize('XXL')}>
+            <p className={`px-3 py-1 cursor-pointer ${sizes.includes('XXL') ? 'bg-pink-300' : 'bg-slate-200'}`}>XXL</p>
           </div>
         </div>
       </div>
