8f5b3352486c8ff7d51c76583430c973c52e3d2a
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-06-30 18:22:22 +0300

    adding the item removing functionality

--- E-commerce-website/admin/src/pages/List.jsx
+++ E-commerce-website/admin/src/pages/List.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { backendUrl, currency } from '../App';
 import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({token}) => {
 
   const [list, setList] = useState([]);
 
@@ -30,6 +30,26 @@ const List = () => {
   useEffect(() => {
     fetchList();
   },[])
+
+  const removeProduct = async (id) => {
+
+    try {
+      
+      const response = await axios.post(backendUrl + '/api/product/remove', {id}, {headers: {token}});
+
+      if(response.data.success) {
+        toast.success(response.data.message);
+        await fetchList();
+      } else {
+        toast.error(response.data.message);
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+
+  }
   
   return (
     <>
@@ -50,12 +70,12 @@ const List = () => {
 
         {
           list.map((item, index) => (
-            <div key={index}>
+            <div className='grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm' key={index}>
               <img className='w-12' src={item.image[0]} alt="product-image" />
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>{currency + item.price}</p>
-              <p>X</p>
+              <p onClick={() => removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
             </div>
           ))
         }
