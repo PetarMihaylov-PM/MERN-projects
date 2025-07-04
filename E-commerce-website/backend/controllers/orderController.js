99adfd6a8fe694655f1341f7dabf973b4960a67e
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-04 11:36:58 +0300

    creating the order routes

--- E-commerce-website/backend/controllers/orderController.js
+++ E-commerce-website/backend/controllers/orderController.js
@@ -16,3 +16,24 @@ const placeOrderStripe = async(req, res) => {
 const placeOrderRazorpay = async(req, res) => {
   
 }
+
+
+// Display all orders for admin panel
+const allOrders = async(req, res) => {
+  
+}
+
+
+// User order data for frontend
+const userOrders = async(req, res) => {
+  
+}
+
+
+// Update order status from admin panel
+const updateStatus = async(req, res) => {
+  
+}
+
+
+export {placeOrder, placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus};
