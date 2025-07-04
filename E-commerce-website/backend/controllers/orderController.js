3507e13d96dcad0bee768510a58fdf3cc3fb8223
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-04 11:23:11 +0300

    Creating the order model for the orders page

---
+++ E-commerce-website/backend/controllers/orderController.js
@@ -0,0 +1,18 @@
+
+
+// Placing orders using COD
+const placeOrder = async(req, res) => {
+
+}
+
+
+// Placing orders using Stripe
+const placeOrderStripe = async(req, res) => {
+  
+}
+
+
+// Placing orders using Razorpay
+const placeOrderRazorpay = async(req, res) => {
+  
+}
