bb954cacc40b5c2ee412180021d642783821414f
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-06 12:15:34 +0300

    rendering the orders information on the admin panel

--- E-commerce-website/admin/src/pages/Orders.jsx
+++ E-commerce-website/admin/src/pages/Orders.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import { backendUrl } from '../App';
 import { toast } from "react-toastify";
+import { assets } from '../assets/assets';
 
 const Orders = ({token}) => {
 
@@ -38,7 +39,29 @@ const Orders = ({token}) => {
 
   return (
     <div>
-      
+      <h3>Order Page</h3>
+      <div>
+        {orders.map((order, index) => (
+          <div key={index}>
+            <img src={assets.parcel_icon} alt="parcel-icon" />
+            <div>
+              {order.items.map((item, index) => {
+                if(index === order.items.length - 1){
+                  return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span> </p>
+                } 
+                else {
+                  return <p key={index}>{item.name} x {item.quantity} <span>{item.size}</span> ,</p>
+                }
+              })}
+            </div>
+            <p>{order.address.firstName + ' ' + order.address.lastName}</p>
+            <div>
+              <p>{order.address.street + ','}</p>
+              <p>{order.address.city + ', ' + order.address.state + ', ' + order.address.country + ', ' + order.address.zipcode}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
