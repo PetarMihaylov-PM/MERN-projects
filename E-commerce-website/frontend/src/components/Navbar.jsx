8fb42bf982baf221f201fe8bd2f2b7365ee18c14
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-09 23:34:19 +0300

    Update Navbar.jsx

--- E-commerce-website/frontend/src/components/Navbar.jsx
+++ E-commerce-website/frontend/src/components/Navbar.jsx
@@ -50,7 +50,7 @@ function Navbar() {
           <img onClick={() => token ? 'null' : navigate('/login')} src={assets.profile_icon} alt="profile-icon" className='w-5 cursor-pointer'/>
           {/* Dropdown Menu */}
           {token && 
-            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+            <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4 z-50'>
               <div className='flex flex-col gap-2 w-36 py-3 px-5  bg-slate-100 text-gray-500 rounded'>
                 <p className='cursor-pointer hover:text-black'>My profile</p>
                 <p onClick={() => navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
