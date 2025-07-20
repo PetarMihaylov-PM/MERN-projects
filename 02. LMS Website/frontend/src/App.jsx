317b180b3625155edf8f9d6f8566e8231651669f
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-20 15:00:16 +0300

    Creating the routes for the pages

--- 02. LMS Website/frontend/src/App.jsx
+++ 02. LMS Website/frontend/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/student/Home';
+import Player from './pages/student/Player';
+import CourseDetails from './pages/student/CourseDetails';
+import CoursesList from './pages/student/CoursesList';
+import MyEnrollments from './pages/student/MyEnrollments';
+import Loading from './components/student/Loading.jsx';
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/course-list' element={<CoursesList />}/>
+        <Route path='/my-enrollments' element={<MyEnrollments />}/>
+        <Route path='/player/:courseId' element={<Player />}/>
+        <Route path='/course/:id' element={<CourseDetails />}/>
+        <Route path='/course-list/:input' element={<CoursesList />}/>
+        <Route path='/loading/:path' element={<Loading />}/>
+      </Routes>
+    </div>
   )
 }
 
