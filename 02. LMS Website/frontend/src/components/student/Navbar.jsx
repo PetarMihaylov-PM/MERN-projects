28ea32e4ce7ae064af5faa96f1e5e5c9513035f2
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-21 16:50:31 +0300

    Integrating clerk authentication

--- 02. LMS Website/frontend/src/components/student/Navbar.jsx
+++ 02. LMS Website/frontend/src/components/student/Navbar.jsx
@@ -8,21 +8,25 @@ const Navbar = () => {
     const isCourseListPage = location.pathname.includes('/course-list');
 
     return (
-        <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-cyan-100/70'}`}>
+        <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-fuchsia-200/70'}`}>
             <img src={assets.logo2} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
 
             <div className='hidden md:flex items-center gap-5 text-gray-500'>
-                <div>
+                <div className='flex items-center gap-5'>
                     <button>Become Educator</button>
-                    <Link to='/my-enrollments'>My Enrollments</Link>
+                    | <Link to='/my-enrollments'>My Enrollments</Link>
                 </div>
-                <button className='bg-blue-600 text-white px-5 py-2 rounded-full'>Create account</button>
+                <button className='bg-fuchsia-700 text-white px-5 py-2 rounded-full'>Create account</button>
 
             </div>
 
             {/* Mobile view */}
-            <div>
-
+            <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
+                <div>
+                    <button>Become Educator</button>
+                    | <Link to='/my-enrollments'>My Enrollments</Link>
+                </div>
+                <button><img src={assets.user_icon} alt="user-icon" /></button>
             </div>
         </div>
     )
