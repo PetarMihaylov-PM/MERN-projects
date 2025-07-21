b1d136087a65df348d69844e292651e3027e4259
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-21 23:04:23 +0300

    Update Navbar.jsx

--- 02. LMS Website/frontend/src/components/student/Navbar.jsx
+++ 02. LMS Website/frontend/src/components/student/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
 
 
     const isCourseListPage = location.pathname.includes('/course-list');
+
+    const { openSignIn } = useClerk();
+    const  { user } = useUser();
 
     return (
         <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-fuchsia-200/70'}`}>
@@ -16,7 +20,7 @@ const Navbar = () => {
                     <button>Become Educator</button>
                     | <Link to='/my-enrollments'>My Enrollments</Link>
                 </div>
-                <button className='bg-fuchsia-700 text-white px-5 py-2 rounded-full'>Create account</button>
+                <button onClick={() => openSignIn()} className='bg-fuchsia-700 text-white px-5 py-2 rounded-full cursor-pointer'>Create account</button>
 
             </div>
 
