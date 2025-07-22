2acf8c6de234090c754eb11aa7c2cc3269790cff
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-22 14:16:21 +0300

    Update CourseCard.jsx

--- 02. LMS Website/frontend/src/components/student/CourseCard.jsx
+++ 02. LMS Website/frontend/src/components/student/CourseCard.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 
-const CourseCard = () => {
+const CourseCard = ({course}) => {
   return (
     <div>
-      <h1>CourseCard</h1>
+      <img src={course.courseThumbnail} alt='thumbnail' />
+      <div>
+        <h3>{course.courseTitle}</h3>
+        <p>{course.educator.name}</p>
+        <div>
+          <p>4.5</p>
+          <div>
+            {[...Array(5)].map((_, i) => (
+              <img key={i} src={assets.star} alt="rating" />
+            ))};
+          </div>
+          <p>
+            22
+          </p>
+        </div>
+        <p>{course.coursePrice - course.discount}</p>
+      </div>
     </div>
   )
 }
