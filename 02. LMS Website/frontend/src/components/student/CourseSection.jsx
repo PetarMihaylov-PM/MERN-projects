cca03e73e8221f510ad24b91de9759d469efa5b6
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-22 11:45:38 +0300

    creating the Companies and CourseSection components

--- 02. LMS Website/frontend/src/components/student/CourseSection.jsx
+++ 02. LMS Website/frontend/src/components/student/CourseSection.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const CourseSection = () => {
   return (
-    <div>
-      <h1>CourseSection</h1>
+    <div className='py-16 md:px-40 px-8'>
+      <h2 className='text-3xl font-medium text-gray-800'>Learn form the best</h2>
+      <p className='text-sm md:text-base text-gray-500 mt-3'>
+        Discover top-rated courses designed to help you learn faster, build real skills, and reach your goals.
+      </p>
+
+      <Link to={'/course-list'} onClick={() => scrollTo(0,0)} className='text-gray-500 border border-gray-500/30 px-10 py-3 rounded'>Show all courses</Link>
     </div>
   )
 }
