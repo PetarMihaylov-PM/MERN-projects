fab3ea851e6eec81480ab08e32c02c31ce1f4418
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-22 16:44:58 +0300

    rendering the course cards on the home page

--- 02. LMS Website/frontend/src/context/AppContext.jsx
+++ 02. LMS Website/frontend/src/context/AppContext.jsx
@@ -1,15 +1,31 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
   const navigate = useNavigate();
+  const [allCourses, setAllCourses] = useState([]);
+  
+  
+  const currency = import.meta.env.VITE_CURRENCY;
 
+  /* Fetch all courses */
+  const getAllCourses = async () => {
+    setAllCourses(dummyCourses);
+  }
+ 
   const value = {
     navigate,
+    currency,
+    allCourses,
   }
+
+  useEffect(() => {
+    getAllCourses();
+  },[]);
 
 
   return(
