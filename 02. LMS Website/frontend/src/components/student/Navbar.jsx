1487b9aa075b61497b7e3e6e4b1cb9162a1b03c5
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-22 10:02:36 +0300

    Update Navbar.jsx

--- 02. LMS Website/frontend/src/components/student/Navbar.jsx
+++ 02. LMS Website/frontend/src/components/student/Navbar.jsx
@@ -12,25 +12,43 @@ const Navbar = () => {
     const  { user } = useUser();
 
     return (
-        <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-fuchsia-200/70'}`}>
+        <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'bg-white' : 'bg-fuchsia-200/50'}`}>
             <img src={assets.logo2} alt="Logo" className='w-28 lg:w-32 cursor-pointer' />
 
             <div className='hidden md:flex items-center gap-5 text-gray-500'>
                 <div className='flex items-center gap-5'>
-                    <button>Become Educator</button>
-                    | <Link to='/my-enrollments'>My Enrollments</Link>
+                    {user && 
+                    <>
+                        <button>Become Educator</button>
+                        | <Link to='/my-enrollments'>My Enrollments</Link>
+                    </>
+                    }
+                    
                 </div>
-                <button onClick={() => openSignIn()} className='bg-fuchsia-700 text-white px-5 py-2 rounded-full cursor-pointer'>Create account</button>
+                { user ? 
+                    <UserButton /> 
+                    :
+                    <button onClick={() => openSignIn()} className='bg-fuchsia-700 text-white px-5 py-2 rounded-full cursor-pointer'>Create account</button>}
 
             </div>
 
             {/* Mobile view */}
             <div className='md:hidden flex items-center gap-2 sm:gap-5 text-gray-500'>
-                <div>
-                    <button>Become Educator</button>
-                    | <Link to='/my-enrollments'>My Enrollments</Link>
+                <div className='flex items-center gap-1 sm:gap-2 max-sm:text-xs'>
+                    {user && 
+                    <>
+                        <button>Become Educator</button>
+                        | <Link to='/my-enrollments'>My Enrollments</Link>
+                    </>
+                    }
                 </div>
-                <button><img src={assets.user_icon} alt="user-icon" /></button>
+                {
+                    user ? 
+                        <UserButton />
+                    :
+                        <button onClick={() => openSignIn()}><img src={assets.user_icon} alt="user-icon" /></button>
+                }
+                
             </div>
         </div>
     )
