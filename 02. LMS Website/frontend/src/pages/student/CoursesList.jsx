95f835afa3d49b85aaf6a696f2f787f90871950d
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-24 10:13:28 +0300

    feat: rendering all courses and adding filtering functionality

--- 02. LMS Website/frontend/src/pages/student/CoursesList.jsx
+++ 02. LMS Website/frontend/src/pages/student/CoursesList.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import AppContext from '../../context/AppContext'
+import SearchBar from '../../components/student/SearchBar';
+import { useParams } from 'react-router-dom';
+import CourseCard from '../../components/student/CourseCard';
 
 function CoursesList() {
+
+  const { navigate, allCourses } = useContext(AppContext);
+  const { input } = useParams();
+  const [filteredCourses, setFilteredCourses] = useState([]);
+
+  useEffect(() => {
+    if(allCourses && allCourses.length > 0) {
+      const tempCourses = allCourses.slice();
+
+      input ? 
+        setFilteredCourses(
+          tempCourses.filter(
+            item => item.courseTitle.toLowerCase().includes(input.toLocaleLowerCase())
+          )
+        )
+      :
+      setFilteredCourses(tempCourses);
+    }
+  },[allCourses, input]);
+
   return (
-    <div>CoursesList</div>
+    <>
+      <div className='relative md:px-36 px-8 pt-20 text-left'>
+        <div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
+          <div>
+            <h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
+            <p className='text-gray-500'>
+              <span onClick={() => navigate('/')} className='text-fuchsia-600 cursor-pointer'>Home</span> / <span>Course List</span>
+            </p>
+          </div>
+          <SearchBar data={input}/>
+        </div>
+        
+        {/* Rendering the Course cards */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
+          {filteredCourses.map((course, index) => <CourseCard key={index} course={course}/>)}
+        </div>
+      </div>
+    </>
   )
 }
 
