14c6c61f8cd165705cb225be6e56d1dd44f4b0ce
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-24 14:13:53 +0300

    Creating the courseDetails page

--- 02. LMS Website/frontend/src/pages/student/CourseDetails.jsx
+++ 02. LMS Website/frontend/src/pages/student/CourseDetails.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import AppContext from '../../context/AppContext';
+import Loading from '../../components/student/Loading';
+import { assets } from '../../assets/assets';
 
 function CourseDetails() {
-  return (
-    <div>CourseDetails</div>
-  )
+
+  const { id } = useParams();
+  const { allCourses, calculateRating } = useContext(AppContext);
+
+  const [courseData, setCourseData] = useState(null);
+  
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find(course => course._id === id);
+    
+    setCourseData(findCourse);
+  }
+
+  useEffect(() => {
+    if (allCourses.length > 0) {
+      fetchCourseData();
+    }
+  }, [allCourses]);
+
+  return courseData ? (
+    <div className='flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left'>
+
+      <div className='absolute top-0 left-0 w-full h-[500px] bg-gradient-to-b from-fuchsia-200/70 '></div>
+
+      {/* Left side */}
+      <div className='max-w-xl z-10 text-gray-500'>
+          <h1 className='md:text-[28px] text-[36px] font-semibold text-gray-800'>{courseData.courseTitle}</h1> 
+          <p className='pt-4 md:text-base text-sm' dangerouslySetInnerHTML={{__html: courseData.courseDescription.slice(0.200)}}></p> 
+
+      {/* Review and ratings */}
+        <div className='flex items-center space-x-2'>
+          <p>{calculateRating(courseData)}</p>
+          <div className='flex'>
+            {[...Array(5)].map((_, i) => (
+              <img className='w-3.5 h-3.5' key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt="rating" />
+            ))}
+          </div>
+          <p className='text-gray-500'>
+            {courseData.courseRatings.length}
+          </p>
+        </div>
+          
+      </div>
+
+
+      {/* Right side */}
+      <div>
+
+      </div>
+    </div>
+  ) 
+  : 
+    <Loading/>
 }
 
 export default CourseDetails
