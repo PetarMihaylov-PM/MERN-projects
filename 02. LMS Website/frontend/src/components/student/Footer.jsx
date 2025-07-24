d1def35e7b6bf05891a7be5b8a7aba67b31502ea
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-24 09:50:58 +0300

    Update Footer.jsx

--- 02. LMS Website/frontend/src/components/student/Footer.jsx
+++ 02. LMS Website/frontend/src/components/student/Footer.jsx
@@ -27,7 +27,14 @@ function Footer() {
 						</li>
 					</ul>
 				</div>
-				<div></div>
+				<div className='hidden md:flex flex-col items-start w-full'>
+					<h2 className='font-semibold text-white mb-5'>Subscribe to our newsletter</h2>
+					<p className='text-sm text-white'>Get the latest course updates, learning tips, and exclusive offers delivered straight to your inbox.</p>
+					<div className='flex items-center pt-4 gap-2'>
+						<input className='border border-gray-500/30 bg-gray-800 text-gray-500 placeholder-gray-500 outline-none w-64 h-9 rounded px-2 text-sm' type="email" placeholder='Enter your email'/>
+						<button className='bg-fuchsia-600 w-24 h-9 text-white rounded'>Subscribe</button>
+					</div>
+				</div>
 			</div>
 			<p className='py-4 text-center text-xs md:text-sm text-white'>Copyright © 2025 Pete's Academy. All Rights Reserved.</p>
 		</footer>
