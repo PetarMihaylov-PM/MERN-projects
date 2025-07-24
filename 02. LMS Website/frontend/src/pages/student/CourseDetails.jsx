e6884dcd4923cfb37bd0d77b7bf5053698c64f82
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-24 14:38:38 +0300

    Update CourseDetails.jsx

--- 02. LMS Website/frontend/src/pages/student/CourseDetails.jsx
+++ 02. LMS Website/frontend/src/pages/student/CourseDetails.jsx
@@ -35,18 +35,21 @@ function CourseDetails() {
           <p className='pt-4 md:text-base text-sm' dangerouslySetInnerHTML={{__html: courseData.courseDescription.slice(0.200)}}></p> 
 
       {/* Review and ratings */}
-        <div className='flex items-center space-x-2'>
-          <p>{calculateRating(courseData)}</p>
-          <div className='flex'>
-            {[...Array(5)].map((_, i) => (
-              <img className='w-3.5 h-3.5' key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt="rating" />
-            ))}
-          </div>
-          <p className='text-gray-500'>
-            {courseData.courseRatings.length}
-          </p>
+      <div className='flex items-center space-x-2 pt-3 pb-1 text-sm'>
+        <p>{calculateRating(courseData)}</p>
+        <div className='flex'>
+          {[...Array(5)].map((_, i) => (
+            <img className='w-3.5 h-3.5' key={i} src={i < Math.floor(calculateRating(courseData)) ? assets.star : assets.star_blank} alt="rating" />
+          ))}
         </div>
-          
+        <p className='text-fuchsia-600'>
+          ({courseData.courseRatings.length} {courseData > 1 ? 'ratings' : 'rating'})
+        </p>
+        <p>{courseData.enrolledStudents.length} {courseData.enrolledStudents.length > 1 ? 'students' : 'student'}</p>
+      </div>
+      
+      <p className='text-sm'>Course by <span className='text-fuchsia-600 underline'>Pete's Academy</span></p>
+        
       </div>
 
 
