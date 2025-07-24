16fd2c7a613d9e8933e5154ca10dfc11e56768c0
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-24 10:20:39 +0300

    Update CoursesList.jsx

--- 02. LMS Website/frontend/src/pages/student/CoursesList.jsx
+++ 02. LMS Website/frontend/src/pages/student/CoursesList.jsx
@@ -3,6 +3,8 @@ import AppContext from '../../context/AppContext'
 import SearchBar from '../../components/student/SearchBar';
 import { useParams } from 'react-router-dom';
 import CourseCard from '../../components/student/CourseCard';
+import { assets } from '../../assets/assets';
+import Footer from '../../components/student/Footer';
 
 function CoursesList() {
 
@@ -38,11 +40,21 @@ function CoursesList() {
           <SearchBar data={input}/>
         </div>
         
+        {/* Search result text */}
+        {input && 
+          <div className='inline-flex items-center gap-4 px-4 py-2 border border-gray-300 rounded mt-8 -mb-8 text-gray-600'>
+            <p>{input}</p>
+            <img className='cursor-pointer' src={assets.cross_icon} alt="cross-icon" onClick={() => navigate('/course-list')}/>
+          </div>
+        }
+
+
         {/* Rendering the Course cards */}
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-16 gap-3 px-2 md:p-0'>
           {filteredCourses.map((course, index) => <CourseCard key={index} course={course}/>)}
         </div>
       </div>
+      <Footer />
     </>
   )
 }
