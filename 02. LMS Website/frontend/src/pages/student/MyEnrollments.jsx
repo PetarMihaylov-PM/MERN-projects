81149519661563b0bbcecfd6d81ead40d9451dc5
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-27 20:23:22 +0300

    Update MyEnrollments.jsx

--- 02. LMS Website/frontend/src/pages/student/MyEnrollments.jsx
+++ 02. LMS Website/frontend/src/pages/student/MyEnrollments.jsx
@@ -1,10 +1,26 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import AppContext from '../../context/AppContext'
 
 function MyEnrollmets() {
 
   const { enrolledCourses, calculateCourseDuration } = useContext(AppContext);
 
+  const [progressArray, setProgressArray] = useState([
+    {lectureCompleted: 2, totalLectures: 4},
+    {lectureCompleted: 1, totalLectures: 5},
+    {lectureCompleted: 3, totalLectures: 6},
+    {lectureCompleted: 4, totalLectures: 4},
+    {lectureCompleted: 0, totalLectures: 3},
+    {lectureCompleted: 5, totalLectures: 7},
+    {lectureCompleted: 6, totalLectures: 8},
+    {lectureCompleted: 2, totalLectures: 6},
+    {lectureCompleted: 4, totalLectures: 10},
+    {lectureCompleted: 3, totalLectures: 5},
+    {lectureCompleted: 7, totalLectures: 7},
+    {lectureCompleted: 1, totalLectures: 4},
+    {lectureCompleted: 0, totalLectures: 2},
+    {lectureCompleted: 5, totalLectures: 5},
+  ]);
 
   return (
     <>
@@ -19,23 +35,23 @@ function MyEnrollmets() {
               <th className='px-4 py-3 font-semibold truncate'>Status</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className='text-gray-700'>
             {enrolledCourses.map((course, index) => (
-              <tr key={index}>
-                <td>
+              <tr key={index} className='border-b border-gray-500/20'>
+                <td className='md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3'>
                   <img src={course.courseThumbnail} alt="course-thumbnail" className='w-14 sm:w-24 md:w-28'/>
-                  <div>
-                    <p>{course.courseTitle}</p>
+                  <div className='flex-1'>
+                    <p className='mb-1 max-sm:text-sm'>{course.courseTitle}</p>
                   </div>
                 </td>
-                <td>
+                <td className='px-4 py-3 max-sm:hidden'>
                   {calculateCourseDuration(course)}
                 </td>
-                <td>
+                <td className='px-4 py-3 max-sm:hidden'>
                   4 / 10 <span>Lectures</span>
                 </td>
-                <td>
-                  <button>Ongoing</button>
+                <td className='px-4 py-3 max-sm:text-right'>
+                  <button className='px-3 sm:px-5 py-1.5 sm:py-2 bg-fuchsia-600 max-sm:text-xs text-white'>Ongoing</button>
                 </td>
               </tr>
             ))}
