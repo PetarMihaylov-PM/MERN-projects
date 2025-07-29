4d2cfb6a7b9032eab25c52bb5616ef20bbf5dd13
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-29 10:52:56 +0300

    Create the educator Navbar

--- 02. LMS Website/frontend/src/pages/educator/Educator.jsx
+++ 02. LMS Website/frontend/src/pages/educator/Educator.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom"
+import Navbar from "../../components/educator/Navbar"
 
 function Educator() {
   return (
-    <div>
-      <h1>Educator page</h1>
+    <div className="text-base min-h-screen bg-white">
+      <Navbar />
       <div>
         {<Outlet />}
       </div>
