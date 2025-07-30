f858ee1e9fbe3d63d7386ce42814909fc0d6c0c6
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-30 23:45:51 +0300

    Update MyCourses.jsx

--- 02. LMS Website/frontend/src/pages/educator/MyCourses.jsx
+++ 02. LMS Website/frontend/src/pages/educator/MyCourses.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import AppContext from '../../context/AppContext'
 
 function MyCourses() {
+
+  const { currency, allCourses } = useContext(AppContext);
+
+  const [courses, setCourses] = useState(null);
+
+  const fetchEducatorCourses = async() => {
+    setCourses(allCourses);
+  }
+
+  useEffect(() => {
+    fetchEducatorCourses();
+  },[])
+
+
   return (
-    <div>MyCourses</div>
+    <div>
+
+    </div>
   )
 }
 
