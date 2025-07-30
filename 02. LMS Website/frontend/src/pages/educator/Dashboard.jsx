dd7cee48222c9920e1bfca14306bcee12adee207
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-30 15:45:16 +0300

    Update Dashboard.jsx

--- 02. LMS Website/frontend/src/pages/educator/Dashboard.jsx
+++ 02. LMS Website/frontend/src/pages/educator/Dashboard.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import AppContext from '../../context/AppContext';
+import { assets, dummyDashboardData } from '../../assets/assets';
+import Loading from '../../components/student/Loading';
 
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+
+  const { currency } = useContext(AppContext);
+  const [dashboardData, setDashboardData] = useState(null);
+
+  const fetchDashboardData = async() => {
+    setDashboardData(dummyDashboardData);
+  }
+
+  useEffect(() => {
+    fetchDashboardData();
+  },[]);
+
+  return dashboardData ? (
+    <div className='min-h-screen flex flex-col items-start justify-between gap-8 md:p-8 md:pb-0 p-4 pt-8 pb-0'>
+      <div className='space-y-5'>
+        <div className='flex items-center gap-3 shadow-card border border-fuchsia-600 p-4 w-56 rounded-md'>
+          <img src={assets.patients_icon} alt="patients-icon" />
+          <div>
+            <p className='text-2xl font-medium text-gray-600'>
+              {dashboardData.enrolledStudentsData.length}
+            </p>
+            <p className='text-base text-gray-500'>Total Enrolments</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  ) 
+  : 
+  <Loading />
 }
 
 export default Dashboard
