8524a03e4d77fdde4d3482c3848181820ebcf2da
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-07-31 11:05:50 +0300

    Render the courses data on My Courses page

--- 02. LMS Website/frontend/src/pages/educator/MyCourses.jsx
+++ 02. LMS Website/frontend/src/pages/educator/MyCourses.jsx
@@ -18,7 +18,7 @@ function MyCourses() {
 
 
   return courses ? (
-    <div className='h-screen flex flex-col items-start justify-between md:p-8 md:pb-8 p-4 pt-8 pb-0'>
+    <div className='min-h-screen flex flex-col items-start justify-between md:p-8 md:pb-8 p-4 pt-8 pb-0'>
       <div className='w-full'>
         <h2 className='pb-4 text-lg font-medium'>My Courses</h2>
         <div className='flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20'>
@@ -42,7 +42,13 @@ function MyCourses() {
                       </span>
                     </td>
                     <td className='px-4 py-3'>
-                      {currency} {Math.floor(course.enrolledStudents.length * (course.coursePrice - course.discount * course.price / 10))}
+                      {currency} {Math.floor(course.enrolledStudents.length * (course.coursePrice - course.discount * course.coursePrice / 100))}
+                    </td>
+                    <td className='px-4 py-3'>
+                      {course.enrolledStudents.length}
+                    </td>
+                    <td className='px-4 py-3'>
+                      {new Date(course.createdAt).toLocaleDateString()}
                     </td>
                   </tr>
                 ))
