1060a0f23f6ca7390057844cefc015e6469567db
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-08-01 14:14:32 +0300

    render Students Enrolled data

--- 02. LMS Website/frontend/src/pages/educator/StudentsEnrolled.jsx
+++ 02. LMS Website/frontend/src/pages/educator/StudentsEnrolled.jsx
@@ -26,6 +26,22 @@ function StudentsEnrolled() {
               <th className='px-4 py-3 font-semibold hidden sm:table-cell'>Date</th>
             </tr>
           </thead>
+          <tbody>
+            {
+              enrolledStudents.map((item, index) => (
+                <tr key={index} className='border-b border-gray-500/20'>
+                  <td className='px-4 py-3 text-center hidden sm:table-cell'>{index + 1}</td>
+                  <td className='md:px-4 px-2 py-3 flex items-center space-x-3'>
+                    <img src={item.student.imageUrl} alt="student-img" className='w-9 h-9 rounded-full'
+                    />
+                    <span className='truncate'>{item.student.name}</span>
+                  </td>
+                  <td className='px-4 py-3 truncate'>{item.courseTitle}</td>
+                  <td className='px-4 py-3 hidden sm:table-cell'>{new Date(item.purchaseDate).toLocaleDateString()}</td>
+                </tr>
+              ))
+            }
+          </tbody>
         </table>
       </div>
     </div>
