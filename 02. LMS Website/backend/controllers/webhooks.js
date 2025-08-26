95c71e4037e6147423c017ed3d7e32ad6ce9e0f1
Petar Mihaylov <petur_mihailovv@abv.bg>
2025-08-26 14:26:43 +0300

    Update webhooks.js

--- 02. LMS Website/backend/controllers/webhooks.js
+++ 02. LMS Website/backend/controllers/webhooks.js
@@ -1,6 +1,8 @@
 import { Webhook } from "svix";
 import User from "../models/user.js";
 import Stripe from "stripe";
+import { Purchase } from "../models/purchase.js";
+import Course from "../models/course.js";
 
 // API controller to manage Clrek User with database
 
@@ -68,10 +70,47 @@ export const stripeWebhooks = async(req, res) => {
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    event = Stripe.webhooks.constructEvent(request.body, sig, process.env.STRIPE_WEBHOOK_SECRET);
   }
   catch (err) {
     response.status(400).send(`Webhook Error: ${err.message}`);
   }
-  
+
+  switch (event.type) {
+    case 'payment_intent.succeeded':{
+      const paymentIntent = event.data.object;
+      const paymentIntentId = paymentIntent.id;
+
+      const session = await stripeInstance.checkout.sessions.list({
+        payment_intent: paymentIntentId,
+      });
+
+      const {purchaseId} = session.data[0].metadata;
+
+      const purchaseData = await Purchase.findById(purchaseId);
+      const userData = await User.findById(purchaseData.userId);
+      const courseData = await Course.findById(purchaseData.courseId.toString());
+
+      courseData.enrolledStudents.push(userData);
+      await courseData.save();
+      userData.enrolledCourses.push(courseData._id);
+      await userData.save();
+
+      purchaseData.status = 'completed';
+      await purchaseData.save();
+
+
+      break;
+    }
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      console.log('PaymentMethod was attached to a Customer!');
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a response to acknowledge receipt of the event
+  response.json({received: true});
 }
